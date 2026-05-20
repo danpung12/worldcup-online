b9c961702e35244700edabc442c2b8e41314c6c1
이은총 <aass6863@naver.com>
2026-05-20 17:39:20 +0900

    chore: prisma db 연결 설정
    - Prisma 초기 설정
    
    - DATABASE_URL 환경변수 연결
    
    - Prisma Client 설정
    
    - DB 연결 확인

---
+++ server/prisma/prisma.service.ts
@@ -0,0 +1,20 @@
+import { Injectable, OnModuleInit } from '@nestjs/common';
+import { PrismaClient } from '../generated/prisma/client';
+import { Pool } from 'pg';
+import { PrismaPg } from '@prisma/adapter-pg';
+
+@Injectable()
+export class PrismaService extends PrismaClient implements OnModuleInit {
+  constructor() {
+    const pool = new Pool({ connectionString: process.env.DATABASE_URL });
+
+    const adapter = new PrismaPg(pool);
+
+    super({ adapter });
+  }
+
+  async onModuleInit() {
+  
+    await this.$connect();
+  }
+}
